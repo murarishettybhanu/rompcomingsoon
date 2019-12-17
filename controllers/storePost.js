f4e4d6ed5b685c7e1ad4d1202f7e9c084b9b35b2
Murarishetty Bhanu Teja <murarishettybhanuteja@gmail.com>
2019-12-17 19:18:12 +0530

    chsnges

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -1,8 +1,10 @@
 const Post = require('../database/models/Post')
 
 module.exports = (req, res) => {
+  console.log('hii');
   var a = req.body;
   var email = a.email;
+  console.log(email);
   Post.create(email, (error, post) => {
     res.redirect('/')
   });
