beb23b9ee5e98d8f7eca1a9598ab8d0681d3c02a
Murarishetty Bhanu Teja <murarishettybhanuteja@gmail.com>
2019-12-17 20:14:34 +0530

    chsnges

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -3,9 +3,5 @@ const Post = require('../database/models/Post')
 module.exports = (req, res) => {
   console.log("hii")
   Post.create(req.body, (error, post) => {
-    if(error){
-      return res.status(400).json({ message: err.message });    
-    }
-    res.json(post)
   });
 }
